28f595e062b59d69458023a9920389dbedcc1e13
Breads <72417429+BreadsRL@users.noreply.github.com>
2021-12-23 17:52:30 -0800

    Create greeting.cpp

---
+++ Advanced_Programming_Labs_[C++]/Assignment_2/greeting.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int main(){
+
+    string x;
+
+    cout << "Enter your name: ";
+    
+    cin >> x;
+
+    cout << "Hello, " << x << "!" << endl;
+
+    return 0;
+}
