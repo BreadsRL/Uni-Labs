a89c7e7b307ab2a6d1998fb40a87143e7a9b24d7
Breads <72417429+BreadsRL@users.noreply.github.com>
2021-12-23 17:59:49 -0800

    Create test.cpp

---
+++ Advanced_Programming_Labs_[C++]/Assignment_3/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "assignment_3.h"
+
+
+using namespace std;
+
+int main() {
+  cout << "Rectange in rectangle: " << rectInRect( 1, 5, 5, 6, 2, 10, 10, 6) << endl;
+  return 0;
+}
